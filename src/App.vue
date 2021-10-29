2f8eb55db79143df44e4e314222750634d592020
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 18:23:09 +0300

    Moved navigation to separated component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/posts">Posts</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="container-fluid">
+    <Navigation/>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
+
+<script>
+import Navigation from '@/components/Navigation.vue';
+
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/posts">Posts</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang='scss'>
+</style>

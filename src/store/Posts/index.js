8ec0e3241c3ccb02fa235cca830c4a7bec2dba80
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 19:41:46 +0300

    Added loading state to Posts store. Removed promise from post deletion action

--- src/store/Posts/index.js
+++ src/store/Posts/index.js
@@ -5,40 +5,41 @@ const postsModule = {
 
   state() {
     return {
+      loaded: false,
       posts: [],
     };
   },
   mutations: {
+    SET_LOADING(state, status) {
+      state.loaded = status;
+    },
     SET_POSTS(state, posts) {
       state.posts = posts;
     },
     DELETE_POST(state, postId) {
-      state.post.splice(state.posts.findIndex((i) => i.id === postId), 1);
+      state.posts.splice(state.posts.findIndex((i) => i.id === postId), 1);
     },
   },
   actions: {
-    fetchPosts({ commit }) {
+    fetchPosts({ commit, state }) {
       return new Promise((resolve, reject) => {
-        axios.get('/posts')
-          .then((response) => {
-            commit('SET_POSTS', response.data);
-            resolve(response);
-          })
-          .catch((error) => {
-            reject(error);
-          });
+        if (!state.loaded) {
+          axios.get('/posts')
+            .then((response) => {
+              commit('SET_POSTS', response.data);
+              commit('SET_LOADING', true);
+              resolve(response);
+            })
+            .catch((error) => {
+              reject(error);
+            });
+        } else {
+          resolve();
+        }
       });
     },
     deletePost({ commit }, postId) {
-      return new Promise((resolve, reject) => {
-        commit('DELETE_POST', postId)
-          .then((response) => {
-            resolve(response);
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
+      commit('DELETE_POST', postId);
     },
   },
   getters: {

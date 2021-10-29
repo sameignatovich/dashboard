29c61d65a48195a3af1286f92c9bc52b1cdaca6f
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 18:12:09 +0300

    Posts page protopype

---
+++ src/views/Posts.vue
@@ -0,0 +1,37 @@
+<template>
+  <table class='table'>
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">User</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for='post in posts' :key='post.id'>
+        <th scope="row">{{ post.id }}</th>
+        <td>{{ post.userId }}</td>
+        <td>{{ post.title }}</td>
+        <td>{{ post.body }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    posts: [],
+  }),
+  mounted() {
+    this.$http.get('/posts')
+      .then((response) => {
+        this.posts = response.data;
+      });
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+</style>

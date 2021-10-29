442c6b4a0f58cf46ead7881886b8276ff4fc5c33
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 20:07:41 +0300

    Added titles for pages

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,25 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+    meta: {
+      title: 'Home',
+    },
   },
   {
     path: '/posts',
     name: 'Posts',
     component: () => import(/* webpackChunkName: "posts" */ '../views/Posts.vue'),
+    meta: {
+      title: 'Posts',
+    },
   },
   {
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      title: 'About',
+    },
   },
 ];
 
@@ -22,6 +31,13 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   linkActiveClass: 'active',
   routes,
+});
+
+router.beforeEach((to) => {
+  const pageTitle = to.meta.title || 'Page';
+  document.title = `${pageTitle} | Dashboard`;
+
+  return true;
 });
 
 export default router;

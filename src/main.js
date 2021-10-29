d1e8a3c0b396c81fbbccdbee4da2176d1ac75ce5
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 17:41:40 +0300

    Added bootstrap. Deleted usuless component

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 
+import 'bootstrap';
+import '@/assets/styles/app.scss';
+
 createApp(App).use(store).use(router).mount('#app');

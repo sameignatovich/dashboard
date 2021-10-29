d644b8fb2815787ddf3a71971878256917191614
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-29 19:04:09 +0300

    Navigation transformed to Navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,9 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/posts">Posts</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary sticky-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Dashboard</a>
+      <button class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link to="/" class='nav-link'>
+              <i class="bi bi-house-fill"></i>
+              Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/posts" class='nav-link'>
+              <i class="bi bi-stickies-fill"></i>
+              Posts
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class='nav-link'>
+              <i class="bi bi-info-square-fill"></i>
+              About
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>

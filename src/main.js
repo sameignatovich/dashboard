dcd13193db70920547e9566d0ca92d630c0cc0ee
Nikita Ignatovich <nikita@ignatovich.me>
2021-10-30 18:38:11 +0300

    Added toasts to project

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import Toaster from '@meforma/vue-toaster';
 import VueAxios from 'vue-axios';
 import axios from './plugins/axios';
 import App from './App.vue';
@@ -13,4 +14,9 @@ createApp(App)
   .use(store)
   .use(router)
   .use(VueAxios, axios)
+  .use(Toaster, {
+    position: 'top',
+    duration: 5000,
+    maxToasts: 3,
+  })
   .mount('#app');

b35a89cc1852b92e9ca9d06e63d9982299a0713f
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-14 22:44:19 +0300

    Added placeholder for comments

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="shadow-lg p-3 bg-body rounded mb-3">
     <h4>Comments</h4>
-    <div  v-for="comment in comments"
-          :key="comment.id"
-          class="m-1 p-1 border-start border-3 border-dark">
-      <div class="comment-header">
-        <router-link :to="`/users/${comment.author.user_id}`" class="fw-bold me-1">
-          <img  :src='comment.author.avatar'
-                class="rounded"
-                height="32" />
-          {{ comment.author.username }}
-        </router-link>
-        <span class="fw-light">
-          {{ $formatdate(comment.created_at, 'dd.mm.yyyy "at" HH:MM') }}
-        </span>
-      </div>
-      <div class="comment-body fw-normal">
-        {{ comment.body }}
+    <div v-if="!loading">
+      <div  v-for="comment in comments"
+            :key="comment.id"
+            class="m-1 p-1 border-start border-3 border-dark">
+        <div class="comment-header">
+          <router-link :to="`/users/${comment.author.user_id}`" class="fw-bold me-1">
+            <img  :src='comment.author.avatar'
+                  class="rounded"
+                  height="32" />
+            {{ comment.author.username }}
+          </router-link>
+          <span class="fw-light">
+            {{ $formatdate(comment.created_at, 'dd.mm.yyyy "at" HH:MM') }}
+          </span>
+        </div>
+        <div class="comment-body fw-normal">
+          {{ comment.body }}
+        </div>
       </div>
     </div>
+    <comments-placeholder v-else />
   </div>
 </template>
 
 <script>
+import CommentsPlaceholder from '@/components/CommentsPlaceholder.vue';
+
 export default {
   props: {
     comments: {
@@ -33,6 +38,9 @@ export default {
       type: Boolean,
       required: true,
     },
+  },
+  components: {
+    CommentsPlaceholder,
   },
 };
 </script>
---
+++ src/components/CommentsPlaceholder.vue
@@ -0,0 +1,25 @@
+<template>
+  <div  v-for="index in 3"
+        :key="index"
+        class="m-1 p-1 border-start border-3 border-dark">
+    <div class="comment-header">
+        <img class="placeholder bg-secondary placeholder-wave rounded me-1"
+             height="32" width="32" />
+        <span class="placeholder col-1 bg-secondary placeholder-wave rounded me-1"></span>
+      <span class="fw-light">
+        <span class="placeholder col-2 bg-secondary placeholder-wave rounded" height="32"></span>
+      </span>
+    </div>
+    <div class="comment-body fw-normal">
+      <div v-for="index in 2" :key="index"
+            class="placeholder col-7 bg-secondary placeholder-wave rounded">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="scss">
+</style>

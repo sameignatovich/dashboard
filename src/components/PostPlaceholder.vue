4cf47be8b7c3953a7c9504114c33dddb5137d566
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-14 20:59:45 +0300

    Changed placeholders widht

--- src/components/PostPlaceholder.vue
+++ src/components/PostPlaceholder.vue
@@ -4,16 +4,16 @@
       <span class="placeholder col-6 bg-secondary placeholder-wave rounded">
       </span>
     </h2>
-      <span class="placeholder col-4 bg-secondary placeholder-wave rounded">
+      <span class="placeholder col-3 bg-secondary placeholder-wave rounded">
       </span>
     <hr>
     <div class='post-body'>
       <span v-for="index in 10" :key="index"
-            class="placeholder col-9 bg-secondary placeholder-wave rounded">
+            class="placeholder col-12 bg-secondary placeholder-wave rounded">
       </span>
     </div>
     <hr>
-    <span class="placeholder col-6 bg-primary placeholder-wave rounded">
+    <span class="placeholder col-4 bg-primary placeholder-wave rounded">
     </span>
   </div>
 </template>

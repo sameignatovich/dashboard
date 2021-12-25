6aa5a06f5da71d7a8e819afa325123b159b703d8
Nikita Ignatovich <nikita@ignatovich.me>
2021-12-25 13:33:14 +0300

    Account activity page prototype

--- src/views/Settings/activity.vue
+++ src/views/Settings/activity.vue
@@ -2,12 +2,97 @@
   <h1 class="text-center">
     Account activity
   </h1>
+  <div class="accordion" id="tokens-an">
+    <div v-for="token in tokens"
+         :key="token.id"
+         class="accordion-item">
+      <h2 class="accordion-header" :id="`tn-${token.id}`">
+        <button class="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                :data-bs-target="`#co-${token.id}`"
+                aria-expanded="true"
+                :aria-controls="`#co-${token.id}`">
+          <span v-if="token.active">
+            <i class="bi bi-check-circle-fill me-1 active-status"></i>
+            <span class="me-1">Last activity: </span>
+          </span>
+          <span v-else>
+            <i class="bi bi-stop-circle-fill me-1 inactive-status"></i>
+            <span class="me-1">Inactive from: </span>
+          </span>
+            {{ $formatdate(token.updated_at) }}
+          <span v-if="token.id == currentTokenId" class="ms-1">
+            <b>(Current session)</b>
+          </span>
+        </button>
+      </h2>
+      <div :id="`co-${token.id}`"
+           class="accordion-collapse collapse"
+           :aria-labelledby="`tn-${token.id}`"
+           data-bs-parent="#tokens-an">
+        <div class="accordion-body">
+          <div class="row">
+            <div class="col-sm-3">
+              <b>IP: </b>
+              {{ token.ip }}
+            </div>
+            <div class="col-sm-3">
+              <b>OS: </b>
+              {{ token.os }}
+            </div>
+            <div class="col-sm-3">
+              <b>Device: </b>
+              {{ token.device }}
+            </div>
+            <div class="col-sm-3">
+              <b>Browser: </b>
+              {{ `${token.browser} ${token.browser_version}` }}
+            </div>
+            <hr class="mt-3" />
+            <div class="col-sm-6">
+              <b>Signed in: </b>
+              {{ $formatdate(token.created_at) }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      tokens: [],
+    };
+  },
+  computed: {
+    currentTokenId() {
+      return this.$store.getters['auth/tokenId'];
+    },
+  },
   beforeMount() {
     this.$title('Account activity');
+    this.fetchTokens();
+  },
+  methods: {
+    fetchTokens() {
+      this.$http.get('/tokens')
+        .then((response) => {
+          this.tokens = response.data.tokens;
+        });
+    },
   },
 };
 </script>
+
+<style scoped lang="scss">
+  .active-status {
+    color: green;
+  }
+  .inactive-status {
+    color: gray;
+  }
+</style>

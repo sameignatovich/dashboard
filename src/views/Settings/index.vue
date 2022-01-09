231be9df3f97be94dbf16bad9302f58307a9cf93
Nikita Ignatovich <nikita@ignatovich.me>
2022-01-10 02:54:55 +0300

    setup icons for menu in account settings

--- src/views/Settings/index.vue
+++ src/views/Settings/index.vue
@@ -3,15 +3,19 @@
     <div class="col-sm-3">
       <nav class="nav nav-pills flex-column mb-3">
         <router-link to="/settings/profile" class="nav-link">
+          <i class="bi bi-person-lines-fill"></i>
           Profile data
         </router-link>
         <router-link to="/settings/avatar" class="nav-link">
+          <i class="bi bi-person-bounding-box"></i>
           Avatar
         </router-link>
         <router-link to="/settings/password" class="nav-link">
+          <i class="bi bi-key-fill"></i>
           Password
         </router-link>
         <router-link to="/settings/activity" class="nav-link">
+          <i class="bi bi-shield-lock-fill"></i>
           Account activity
         </router-link>
       </nav>
